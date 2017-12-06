15ab6029f43875b1d0dceef3ddae96d9a98a7313
ГлаватскихД <@227_4>
2017-12-06 09:34:18 +0600

    Add project files.

---
+++ ConsoleApplication9/Source.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include <locale.h>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+
+	int x;
+	cin >> x;
+	switch (x)
+	{
+		case 6:
+		{
+			cout << x;
+		}
+		break;
+
+		//else
+		default:
+		{
+
+		}
+		break;
+	}
+}
